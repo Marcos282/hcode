58961a5f3e449373bfa3576bb91f12136df3caa7
Marcos Sousa <novahost@gmail.com>
2023-05-28 14:14:19 -0300

    teste

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php
 
+
 echo json_encode($marcos = array(
     "Nome"=>"Marcos aurelio",
+    "Casado"=>"SIM",
     "Idade"=>"44 anos",
     "Sexo"=>array("fator"=>"B","type"=>"x"),
     "email"=>"novahost@gmail.com"
 ));
+ 
 
 #var_dump($marcos);
